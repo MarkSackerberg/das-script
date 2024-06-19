91e6df7947d483557cfed038ae5884810b47eb94
MarkSackerberg <93528482+MarkSackerberg@users.noreply.github.com>
2024-06-20 00:13:57 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import { dasApi } from "@metaplex-foundation/digital-asset-standard-api"
+import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
+
+import { das }  from '@metaplex-foundation/mpl-core-das';
+
+
+import { publicKey } from '@metaplex-foundation/umi';
+
+const assetId = publicKey('FgEKkVTSfLQ7a7BFuApypy4KaTLh65oeNRn2jZ6fiBav');
+const collectionId = publicKey('FgEKkVTSfLQ7a7BFuApypy4KaTLh65oeNRn2jZ6fiBav');
+const wallet = publicKey('AUtnbwWJQfYZjJ5Mc6go9UancufcAuyqUZzR1jSe4esx');
+
+(async () => {
+    const umi = createUmi("https://solana-devnet.rpc.extrnode.com/47b2966e-f6b5-4f8d-9c2e-c48a77f2448b").use(dasApi());
+    const asset = await umi.rpc.getAsset(assetId);
+
+    console.log("on chain Asset");
+    console.log(asset);
+
+    const collection = await umi.rpc.getAsset(collectionId);
+
+    console.log("on chain collection");
+    console.log(collection);
+
+    const dasAsset = await das.searchAssets(umi, {
+      owner: publicKey(wallet),
+      interface: 'MplCoreAsset',
+    })
+    console.log("dasAsset")
+    console.log(dasAsset)
+  })();
